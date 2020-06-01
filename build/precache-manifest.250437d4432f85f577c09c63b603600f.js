4c0d89f4944f4d8afc94b989d84b0dadebfb4151
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-06-01 11:32:15 +0700

    Tugas_2_reactjs
    Mengumpulkan tugas 2 reactjs

---
+++ build/precache-manifest.250437d4432f85f577c09c63b603600f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35951cdf2b4332d19fbab7eb683595d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "eed93c9a0c32ea3ad2ae",
+    "url": "/static/js/main.86c77a5b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
